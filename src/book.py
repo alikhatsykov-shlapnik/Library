a98cd683beea1d81242618578f3f25c14eb0df1e
Alik <alikhatsykov@gmail.com>
2026-07-08 20:45:19 +0300

    organs

--- src/book.py
+++ src/book.py
@@ -1,27 +1,27 @@
 class Book:
     """Класс для предоставления книги"""
 
-    def __init__(self, title: str, auther: str, year: int, book_id: int = None, status: str = "В наличии"):
+    def __init__(self, title: str, author: str, year: int, book_id: int = None, status: str = "В наличии"):
         self.id = book_id
         self.title = title
-        self.auther = auther
+        self.auther = author
         self.year = year
         self.status = status
 
     def __str__(self):
-        """Строковое представление книги (для print)"""
+        """Для пользователей"""
         return f"[{self.id}], {self.title} - {self.auther} ({self.year}) - {self.status}"
 
     def __repr__(self):
-        """Представление для Разработчиков"""
-        return f"Book(id = {self.id}, title = '{self.title}', author = '{self.auther}', year = '{self.year}', status = '{self.status}')"
+        """Для Разработчиков"""
+        return f"Book(id = {self.id}, title = '{self.title}', author = '{self.author}', year = '{self.year}', status = '{self.status}')"
     
     def to_dict(self):
         """превращает книгу в словарь"""
         return {
             "id": self.id,
             "title": self.title,
-            "author": self.auther,
+            "author": self.author,
             "year": self.year,
             "status": self.status
         }
--- src/database.py
+++ src/database.py
@@ -1,3 +1,47 @@
 import sqlite3
 import os
 
+DB_PATH = "data/library.db"
+
+def get_connection():
+    """
+    Создает и возвращает подключение к базе данных SQLite.
+    Создаёт папку data, при её отсутствии.
+    """
+
+    #create folder "data" if It isn't
+    os.makedirs("data", exist_ok = True)
+
+    return sqlite3.connect(DB_PATH)
+
+def init_database():
+    """
+    Create table "bools", if it isn't.
+    This function open once at startup programm.
+    """
+
+    #get connection
+    conn = get_connection()
+
+    #Create cursor - the tool for make reqests
+    cursor = conn.cursor()
+
+    #SQL - reqest for create table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXIST books (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            author TEXT NOT NULL,
+            year INTEGER NOT NULL,
+            status TEXT DEFAULT 'в наличии'
+        )
+""")
+    
+    #save changes
+    conn.commit()
+
+    #close connection
+    conn.close()
+
+    print("База данных инициализирована")
+
